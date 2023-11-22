3e8569a98f76ed9c638e846621259b1494c8a2fb
Dziomek <dziomek@student.agh.edu.pl>
2023-11-22 14:38:47 +0100

    fixed missing language bindings in PersonalSurvey

--- src/views/PersonalSurvey.vue
+++ src/views/PersonalSurvey.vue
@@ -20,7 +20,7 @@
                 <div class="flex items-center gap-16 w-full flex-wrap">
                     <div v-for="data in ages" :key="data.key" class="flex gap-2">
                         <RadioButton v-model="selectedAge" :inputId="`${data.key}`" name="dynamic" :value="data.value" />
-                        <label :for="`${data.key}`">{{ data.value }}</label>
+                        <label :for="`${data.key}`">{{ data.labels ? language == 'en' ? data.labels.en : data.labels.pl : data.value }}</label>
                     </div>
                 </div>
             </div>
@@ -39,7 +39,7 @@
                 <div class="flex items-center gap-16 w-full flex-wrap">
                     <div v-for="data in genderData" :key="data.key" class="flex gap-2">
                         <RadioButton v-model="selectedGender" :inputId="`${data.key}`" name="dynamic" :value="data.value" />
-                        <label :for="`${data.key}`">{{ data.value }}</label>
+                        <label :for="`${data.key}`">{{ data.labels ? language == 'en' ? data.labels.en : data.labels.pl : data.value }}</label>
                     </div>
                 </div>
             </div>
@@ -74,8 +74,7 @@
                 </div>
                 <div class="flex items-center gap-16 w-full flex-wrap">
                     <div :key="1" class="flex gap-2">
-                        <RadioButton v-model="selectedMyopia" @change="impairmetsInputChange" inputId="1" name="dynamic"
-                            :value="true" />
+                        <Checkbox v-model="selectedMyopia" @change="impairmetsInputChange" inputId="1" :binary="true" />
                         <label for="1">
                             {{ language == 'en'
                                 ? 'Myopia'
@@ -84,8 +83,7 @@
                         </label>
                     </div>
                     <div :key="2" class="flex gap-2">
-                        <RadioButton v-model="selectedForesight" @change="impairmetsInputChange" inputId="2" name="dynamic"
-                            :value="true" />
+                        <Checkbox v-model="selectedForesight" @change="impairmetsInputChange" inputId="2" :binary="true" />
                         <label for="2">
                             {{ language == 'en'
                                 ? 'Foresight'
@@ -94,8 +92,7 @@
                         </label>
                     </div>
                     <div :key="3" class="flex gap-2">
-                        <RadioButton v-model="selectedDaltonism" @change="impairmetsInputChange" inputId="3" name="dynamic"
-                            :value="true" />
+                        <Checkbox v-model="selectedDaltonism" @change="impairmetsInputChange" inputId="3" :binary="true" />
                         <label for="3">
                             {{ language == 'en'
                                 ? 'Daltonism'
@@ -104,8 +101,7 @@
                         </label>
                     </div>
                     <div :key="4" class="flex gap-2">
-                        <RadioButton v-model="selectedSpatialBlindness" @change="impairmetsInputChange" inputId="4"
-                            name="dynamic" :value="true" />
+                        <Checkbox v-model="selectedSpatialBlindness" @change="impairmetsInputChange" inputId="4" :binary="true" />
                         <label for="4">
                             {{ language == 'en'
                                 ? 'Spatial blindness'
@@ -114,8 +110,7 @@
                         </label>
                     </div>
                     <div :key="5" class="flex gap-2">
-                        <RadioButton v-model="selectedNoVisualImpairment" @change="noImpairmentInputChange" inputId="5"
-                            name="dynamic" :value="true" />
+                        <RadioButton v-model="selectedNoVisualImpairment" @change="noImpairmentInputChange" inputId="5" :value="true" />
                         <label for="5">
                             {{ language == 'en'
                                 ? 'No visual impairment'
@@ -218,9 +213,10 @@ const selectedDaltonism = ref(false)
 const selectedSpatialBlindness = ref(false)
 const selectedNoVisualImpairment = ref(false)
 
-const genderData = [{ key: 1, value: language == 'en' ? 'Male' : 'Mężczyzna' }, { key: 2, value: language == 'en' ? 'Female' : 'Kobieta' },
-{ key: 3, value: language == 'en' ? 'Do not want to give' : 'Nie chcę podawać' }]
-const ages = [{ key: 1, value: language == 'en' ? 'Under 18' : 'Poniżej 18' }, { key: 2, value: '18-25' }, { key: 3, value: '25-30' }, { key: 4, value: '30-35' },
+const genderData = [{ key: 1, value: 'Male', labels: { en: 'Male', pl: 'Mężczyzna' } }, 
+{ key: 2, value: 'Female', labels: { en: 'Female', pl: 'Kobieta' } }, 
+{ key: 3, value: 'Do not want to give', labels: { en: 'Do not want to give', pl: 'Nie chcę podawać' } }]
+const ages = [{ key: 1, value: 'Under 18', labels: { en: 'Under 18', pl: 'Poniżej 18' } }, { key: 2, value: '18-25' }, { key: 3, value: '25-30' }, { key: 4, value: '30-35' },
 { key: 5, value: '40-50' }, { key: 6, value: '50-60' }, { key: 7, value: '60-70' }]
 const lensesOrGlasses = [{ key: 1, value: true }, { key: 2, value: false }]
 
