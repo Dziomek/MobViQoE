543add87e37c1848cb209c6b009ac144c239a6a1
Dziomek <dziomek@student.agh.edu.pl>
2023-12-23 18:34:05 +0100

    try new src

--- src/videoConfig.js
+++ src/videoConfig.js
@@ -4,7 +4,7 @@ export const VIDEO_CONFIG = [
     // { index: 2, title: 'vid2', src: 'https://upload.wikimedia.org/wikipedia/commons/2/2e/Badger_collecting_bedding_material.webm'},
     // { index: 3, title: 'vid3', src: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Squirrel_eating_cicada.ogg'},
     // { index: 4, title: 'vid4', src: 'https://upload.wikimedia.org/wikipedia/commons/8/8b/2021-08-11%2C_Bison_crosses_the_road%2C_Yellowstone_National_Park.webm'},
-    { index: 1, title: 'RUST_beach', src: new URL('../videos/rust_beach_excellent.mp4', import.meta.url).href},
+    { index: 1, title: 'RUST_beach', src: 'https://mobviqoe-bucket.s3.eu-central-1.amazonaws.com/rust_beach_excellent.mp4'},
 ]
 
 export const VIDEOS_NUMBER = VIDEO_CONFIG.length
